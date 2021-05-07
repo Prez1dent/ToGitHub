0d626109bf7b8136871c6ac403ac1272116bb93a
Artur <melkonyanartur@mail.ru>
2021-05-07 16:02:37 +0300

    New2

--- ToGitHub/ViewController.swift
+++ ToGitHub/ViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-let 
+let
+    let num
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
